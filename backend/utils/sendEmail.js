018bfeae5d1ce698edb92a08438719a40e73f42f
ashwanibaghel <ashwanibaghel826@gmail.com>
2025-09-15 22:24:51 +0530

    Update email transport configuration for Zoho

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,9 +2,9 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.zoho.in",
-  port: 465,
-  secure: true,
+  host: "smtp.zoho.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
